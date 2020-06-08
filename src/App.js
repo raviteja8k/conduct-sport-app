51f8b5e5dd5569dbe8dadef251b75dfd479b5992
raviteja8k <raviteja.tk@gmail.com>
2020-06-08 20:27:45 +0530

    Trying CSS modules to style

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './app.module.css';
 import Players from './Players/Players';
 import Pairs from './Pairs/Pairs';
 
 class App extends Component {
   state = {
-    playerNames: [],
-    pairs: []
+    playerNames: ["Sean","Kyle","Emily","Nick","Cotter","Brian","Jeremy","Kimmy","Pat","Johnny"],
+    pairs: ["Sean","Kyle","Emily","Nick","Cotter","Brian","Jeremy","Kimmy","Pat","Johnny"]
     
   };
 
   addPlayer = (event) => {
-    const newPlayers = [...this.state.playerNames];
-    if(event.key === 'Enter') {newPlayers.push(event.target.value)};
+    const newPlayers2 = [...this.state.playerNames];
+    if(event.key === 'Enter') {newPlayers2.push(event.target.value)};
     //this.clearValue = event.target.value;
-    this.setState({playerNames: newPlayers});
+    this.setState({playerNames: newPlayers2});
   
 
   }
 
   pairPlayers = () => {
-    console.log('pairs');
+    const names = [...this.state.playerNames];
+   // let pickpool = []; // Slice the array at the first element to copy it by value
+
+    if (names.length % 2 !== 0) {
+      alert("You must have an even number of names. You currently have " + names.length + " names.");
+  } else {
+      var arr1 = names.slice(), // copy array
+          arr2 = names.slice(); // copy array again
+  
+      arr1.sort(function() { return 0.5 - Math.random();}); // shuffle arrays
+      arr2.sort(function() { return 0.5 - Math.random();});
+  
+      while (arr1.length) {
+          var name1 = arr1.pop(), // get the last value of arr1
+              name2 = arr2[0] === name1 ? arr2.pop() : arr2.shift();
+              //        ^^ if the first value is the same as name1, 
+              //           get the last value, otherwise get the first
+  
+          console.log(name1 + ' gets ' + name2 );
+        //  pickpool = (name1 + ' gets ' + name2);
+      }
+  }
+ // this.setState({pairs: picks});  
   }
 
   render(){
     return(
-    <div className="App">
+    <div className={styles.App} >
       <h1>Conduct Sports !</h1>
         Enter player names here: <input onKeyUp={(e) => this.addPlayer(e)} />
        { this.state.playerNames.map((pnames, pkey) =>
@@ -36,8 +58,8 @@ class App extends Component {
         <button >Set Rival</button>
         </div>
         <div style={{display: 'block'}}>
-          {this.state.pairs.map((pairs, pairId) => 
-            <Pairs pairNames={pairs} key={pairId}/>
+          {this.state.pairs.map((npairs, pairId) => 
+            <Pairs pairNames={npairs} key={pairId}/>
           )
           }
         </div>
